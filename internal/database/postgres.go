36feb0fdf2bc7796ddc5ccd87c5345c362b20a9f
Chetan <sachanchetan646@gmail.com>
2026-07-02 12:35:09 +0530

    database connect function

--- internal/database/postgres.go
+++ internal/database/postgres.go
@@ -1,1 +1,35 @@
 package database
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func Connect(databaseURL string) (*pgxpool.Pool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	config, err := pgxpool.ParseConfig(databaseURL)
+	if err != nil {
+		log.Printf("Unable to parse DATABASE_URL: %v", err)
+		return nil, err
+	}
+
+	pool, err := pgxpool.NewWithConfig(ctx, config)
+	if err != nil {
+		log.Printf("Unable to connect to database: %v", err)
+		return nil, err
+	}
+
+	err2 := pool.Ping(ctx)
+	if err2 != nil {
+		log.Printf("Unable to ping database: %v", err)
+		return nil, err
+	}
+
+	log.Printf("Database connected to: %v", databaseURL)
+	return pool, nil
+}
